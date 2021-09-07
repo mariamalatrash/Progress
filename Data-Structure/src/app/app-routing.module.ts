710776ede79c997d99d129cc74b59ebe0cba60ff
mariamalatrash <mariamalatrash1@gmail.com>
2021-09-07 16:11:55 +0300

    adding routing

--- Data-Structure/src/app/app-routing.module.ts
+++ Data-Structure/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StackComponent} from '../app/stack/stack.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'stack' , component:StackComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
