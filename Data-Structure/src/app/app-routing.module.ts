56587101636b48dbc5170d71edfd3657509a56f4
mariamalatrash <mariamalatrash1@gmail.com>
2021-09-09 13:10:50 +0300

    add routing to queue page

--- Data-Structure/src/app/app-routing.module.ts
+++ Data-Structure/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {StackComponent} from '../app/stack/stack.component';
+import { QueueComponent } from './queue/queue.component';
 
 
 const routes: Routes = [
-  {path:'stack' , component:StackComponent} 
+  {path:'stack' , component:StackComponent},
+  {path:'queue' ,component:QueueComponent}
 ];
 
 @NgModule({
