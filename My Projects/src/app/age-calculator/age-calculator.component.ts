4a9cb6acb08f8ed1e2ee2f82548fc6c0a2a7414c
mariamalatrash <mariamalatrash1@gmail.com>
2021-09-12 19:01:30 +0300

    modified age calculator

--- My Projects/src/app/age-calculator/age-calculator.component.ts
+++ My Projects/src/app/age-calculator/age-calculator.component.ts
@@ -1,19 +1,34 @@
-import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
-import { ajaxPrefilter } from 'jquery';
-
+import { Component, OnInit, } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-age-calculator',
   templateUrl: './age-calculator.component.html',
   styleUrls: ['./age-calculator.component.css']
 })
+
+
 export class AgeCalculatorComponent implements OnInit {
+  birthday!:Date;
+  Age!:string;
 
-  constructor() {  }
+    constructor() {
 
+    }
+    calc(){
+    const day=this.birthday.getDate();
+    const month=this.birthday.getMonth();
+    const year=this.birthday.getFullYear();
+  
+    let timeDiff = Math.abs(Date.now() - this.birthday.getTime());
+    let age = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
+
+    this.Age= "your are "+ age +" years old" ;
+        }
   ngOnInit(): void {
+    
   }
-
+  
+  
 }
 
 
