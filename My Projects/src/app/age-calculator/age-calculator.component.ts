9b120eb212dc2603daf1ec159f71d14ad0644c8c
mariamalatrash <mariamalatrash1@gmail.com>
2021-09-16 12:39:09 +0300

    add calculate month

--- My Projects/src/app/age-calculator/age-calculator.component.ts
+++ My Projects/src/app/age-calculator/age-calculator.component.ts
@@ -10,19 +10,31 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AgeCalculatorComponent implements OnInit {
   birthday!:Date;
   Age!:string;
-
+  age:number | undefined;
     constructor() {
 
     }
     calc(){
-    const day=this.birthday.getDate();
-    const month=this.birthday.getMonth();
+    const dayBirth=this.birthday.getDate();
+    const monthBirth=this.birthday.getMonth()+1;
     const year=this.birthday.getFullYear();
-  
+    let Date1=new Date;
+    let dateNow=Date1.getFullYear();
+    if(year<dateNow){
     let timeDiff = Math.abs(Date.now() - this.birthday.getTime());
-    let age = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
-
-    this.Age= "your are "+ age +" years old" ;
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+  }
+    else{
+      alert("Not Exist!!")
+    }
+    let monthNow=Date1.getMonth()+1;
+    let month;
+    if(monthBirth>monthNow)
+      month=12-monthBirth+monthNow;
+    else{
+      month=monthNow-monthBirth;
+    }
+    this.Age="your age is "+this.age+" year and " +month+ "months";
         }
   ngOnInit(): void {
     
